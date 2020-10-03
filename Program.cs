d5bd2c59a36c3e51790b9d2ffd1bad19b2a09afa
F1nka0 <Riainar@bk.ru>
2020-10-03 21:40:28 +0300

    b

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CryptoLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vigenere vig = new Vigenere();
+            vig.SolveVigenere("program","dog");
+        }
+    }
+}
