b3a0983d378d822bb88ea70e58cbb4a995115aa8
F1nka0 <Riainar@bk.ru>
2020-10-03 21:52:10 +0300

    vigenere algo added

---
+++ Vigenere.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CryptoLib
+{
+
+    class Vigenere
+    {
+        static string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public void SolveVigenere(string SourceString, string Key)
+        {
+
+            GetDigitalEquivalentForKey("dog", SourceString.Length);
+        }
+        private string GetStringEquivalentOf
+        private string GetDigitalEquivalentForKey(string RawKey, int SourceStringLength)
+        {
+
+            RawKey.ToUpper();
+            string ComputedKey = "";
+
+            while (RawKey.Length < SourceStringLength)
+            {
+
+                RawKey += RawKey;
+            }
+            RawKey = RawKey.Substring(0, SourceStringLength).ToUpper();
+
+            
+            return ComputedKey;
+        }
+        private string GetEncodedValue(string ComputedKey,) {
+
+            string EncodedValue = "";
+            
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+            //парфёнов--------------------------------------------------------------------------------------
+        }
+    }
+}
